2f74898aaabf1e38862763e51a1931a3503a02e7
Jacko Dirks <jacko.dirks@gmail.com>
2021-02-07 17:39:21 +0100

    raspberrypi: Rework code for multiple led matrices

--- raspberrypi/src/main.c
+++ raspberrypi/src/main.c
@@ -9,7 +9,7 @@
 #include "pabort.h"
 #include "fontTranslation.h"
 
-static const size_t colCount = 8;
+static const size_t colCount = 16;
 static atomic_bool halt = false;
 
 static void intHandler(int i) {
@@ -17,7 +17,7 @@ static void intHandler(int i) {
     halt = true;
 }
 
-static const char* text = "Hello, world! ";
+static const char* text = "Woooww, dat is gaaf";
 
 int main(void)
 {
@@ -28,54 +28,65 @@ int main(void)
     if (!startLedDriving(colCount)){
         pabort("startLedDriving failed");
     }
-    // Translate the text
-    size_t textLen = strlen(text);
-    if (textLen < colCount)
-        textLen = colCount;
-    const size_t tileCount = textLen * TILE_SIZE_ROWS_COLS;
-    uint_fast8_t* tiles = calloc(textLen, TILE_SIZE_BYTE);
+    // Translate the text. We need at least as many text tiles as output tiles,
+    // so make sure that is the case.
+    size_t textTilesCount = strlen(text);
+    const size_t textTileArrLen = textTilesCount * TILE_SIZE_ROWS_COLS;
+    const size_t outputTilesCount = colCount/TILE_SIZE_ROWS_COLS;
+    uint_fast8_t* textTiles = calloc(textTileArrLen, sizeof(uint_fast8_t));
     for (size_t i = 0; i < strlen(text); ++i) {
         const uint_fast8_t* p = charToTile(text[i]);
-        memcpy(&tiles[i * TILE_SIZE_ROWS_COLS], p, TILE_SIZE_BYTE);
+        memcpy(&textTiles[i * TILE_SIZE_ROWS_COLS], p, TILE_SIZE_BYTE);
     }
-    // Prepare one tile
-    uint_fast8_t* tile = calloc((colCount / LED_MATRIX_ROW_COUNT) * LED_MATRIX_ROW_COUNT, sizeof(tiles[0]));
-    memcpy(tile, tiles, colCount*sizeof(tiles[0]));
-    size_t rowIndex = 8;
+    // Prepare the output tiles
+    uint_fast8_t* outputTiles = calloc(LED_MATRIX_ROW_COUNT*outputTilesCount, sizeof(textTiles[0]));
+    size_t *rowIndex = calloc(outputTilesCount, sizeof(size_t));
+    {
+        size_t tileStart = 0;
+        for (size_t i = 0; i < outputTilesCount; ++i) {
+            memcpy(&outputTiles[i*LED_MATRIX_ROW_COUNT], &textTiles[tileStart], LED_MATRIX_ROW_COUNT * sizeof(outputTiles[0]));
+            tileStart += LED_MATRIX_ROW_COUNT;
+            if (tileStart > textTilesCount) {
+                tileStart = 0;
+            }
+            rowIndex[i] = tileStart;
+        }
+
+    }
     size_t bitIndex = 0;
     unsigned int curBrightness = 100;
-    bool brightnessFalling = true;
     ledDriverSetBrightnessPercentage(curBrightness);
 
     struct timespec deadline;
     clock_gettime(CLOCK_MONOTONIC, &deadline);
     while(!halt) {
         uint_fast8_t *b = ledDriverGetInactiveBuffer();
-        memcpy(b, tile, sizeof(b[0])*8);
+        for (size_t row = 0; row < LED_MATRIX_ROW_COUNT; ++row) {
+            for (size_t rStart = 0; rStart < outputTilesCount; ++rStart) {
+                size_t actTile = outputTilesCount - 1 - rStart;
+                b[row * outputTilesCount + rStart] = outputTiles[row + actTile*LED_MATRIX_ROW_COUNT];
+            }
+        }
         ledDriverSwapBuffer();
         // Update our tile
-        // TODO fix this for colcount > 8
-        for (size_t i = 0; i < LED_MATRIX_ROW_COUNT; ++i) {
-            uint_fast8_t newRow = tiles[rowIndex + i];
-            newRow >>= bitIndex;
-            tile[i] = tile[i] >> 1 | newRow << 7;
+        for (size_t j = 0; j < outputTilesCount; ++j) {
+            for (size_t i = 0; i < LED_MATRIX_ROW_COUNT; ++i) {
+                uint_fast8_t newRow = textTiles[rowIndex[j] + i];
+                newRow >>= bitIndex;
+                outputTiles[i + j*LED_MATRIX_ROW_COUNT] = outputTiles[i + j*LED_MATRIX_ROW_COUNT] >> 1 | newRow << 7;
+            }
         }
         bitIndex++;
-        if (bitIndex >= 8) {
+        if (bitIndex >= LED_MATRIX_ROW_COUNT) {
             bitIndex = 0;
-            rowIndex += 8;
-            if (rowIndex >= tileCount) {
-                rowIndex = 0;
-            }
-            // Update brightness
-            curBrightness += brightnessFalling ? -2 : +2;
-            ledDriverSetBrightnessPercentage(curBrightness);
-            if (curBrightness == 0) {
-                brightnessFalling = false;
-            } else if (curBrightness == 100) {
-                brightnessFalling = true;
+            for (size_t i = 0; i < outputTilesCount; ++i) {
+                rowIndex[i] += LED_MATRIX_ROW_COUNT;
+                if (rowIndex[i] >= textTileArrLen) {
+                    rowIndex[i] = 0;
+                }
             }
         }
+
         deadline.tv_nsec += 40000000;
         if(deadline.tv_nsec >= 1000000000) {
             deadline.tv_nsec -= 1000000000;
@@ -89,7 +100,8 @@ int main(void)
     ledDriverSwapBuffer();
 
     stopLedDriving();
-    free(tiles);
-    free(tile);
+    free(textTiles);
+    free(outputTiles);
+    free(rowIndex);
     return EXIT_SUCCESS;
 }
