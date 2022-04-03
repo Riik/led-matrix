67545ba7bd5cccf44c34fb0bf213e27c48a7e8ed
Jacko Dirks <jacko.dirks@gmail.com>
2022-04-03 22:38:46 +0200

    v2:rpi:FrameLimiter: Fix public

--- v2/raspberrypi/inc/frameLimiter.hpp
+++ v2/raspberrypi/inc/frameLimiter.hpp
@@ -1,16 +1,14 @@
 #include <chrono>
 
 class FrameLimiter {
+    public:
+        /* zero is a special case, in that case there will be no sleeping. */
+        FrameLimiter(const uint32_t framesPerSecond);
 
-    /* zero is a special case, in that case there will be no sleeping. */
-    FrameLimiter(const unsigned int framesPerSecond);
+        /* Puts the calling thread to sleep until the next frame. */
+        void waitForNextFrame();
 
-    /* Puts the calling thread to sleep until the next frame. */
-    void waitForNextFrame();
-
-private:
-    std::chrono::time_point<std::chrono::steady_clock> lastWakeTime;
-    std::chrono::nanoseconds timePerFrame;
+    private:
+        std::chrono::time_point<std::chrono::steady_clock> lastWakeTime;
+        std::chrono::nanoseconds timePerFrame;
 };
-
-
