b73bb949cebf5d50a6f913f01d975f6d9ab5dab6
Jacko Dirks <jacko.dirks@gmail.com>
2022-04-03 20:19:32 +0200

    v2:rpi:MatrixScreen: Guard operator() against out of range

--- v2/raspberrypi/src/matrixScreen.cpp
+++ v2/raspberrypi/src/matrixScreen.cpp
@@ -1,4 +1,6 @@
 #include <algorithm>
+#include <stdexcept>
+#include <sstream>
 
 #include "matrixScreen.hpp"
 
@@ -18,12 +20,32 @@ void MatrixScreen::resetScreen(MatrixScreen::PixelColor color)
 
 MatrixScreen::PixelColor MatrixScreen::operator()(std::size_t width, std::size_t height) const
 {
+    if (width >= this->pixelCountWidth) {
+        std::stringstream ss;
+        ss << "width out of range: max: " << this->pixelCountWidth << " got " << width;
+        throw std::out_of_range(ss.str());
+    }
+    if (height >= this->pixelCountHeight) {
+        std::stringstream ss;
+        ss << "height out of range: max: " << this->pixelCountHeight << " got " << height;
+        throw std::out_of_range(ss.str());
+    }
     return this->screen.at(this->pixelCountWidth*height + width);
 }
 
 
 MatrixScreen::PixelColor& MatrixScreen::operator()(std::size_t width, std::size_t height)
 {
+    if (width >= this->pixelCountWidth) {
+        std::stringstream ss;
+        ss << "width out of range: max: " << this->pixelCountWidth << " got " << width;
+        throw std::out_of_range(ss.str());
+    }
+    if (height >= this->pixelCountHeight) {
+        std::stringstream ss;
+        ss << "height out of range: max: " << this->pixelCountHeight << " got " << height;
+        throw std::out_of_range(ss.str());
+    }
     return this->screen.at(this->pixelCountWidth*height + width);
 }
 
