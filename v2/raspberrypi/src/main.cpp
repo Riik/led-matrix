27f5840c7c52ecf869c6a823f8d6715a257c5000
Jacko Dirks <jacko.dirks@gmail.com>
2022-05-29 20:50:59 +0200

    v2:rpi:Rotating square!

--- v2/raspberrypi/src/main.cpp
+++ v2/raspberrypi/src/main.cpp
@@ -1,9 +1,13 @@
 #include <atomic>
 #include <csignal>
+#include <vector>
+#include <chrono>
 
 #include "matrixScreen.hpp"
 #include "matrixDriver.hpp"
-#include "gameOfLife.hpp"
+
+#include "canvas.hpp"
+#include "solidColorTriangle2d.hpp"
 
 const std::string spidev = "/dev/spidev0.0";
 static std::atomic_bool halt = false;
@@ -13,23 +17,49 @@ static void sigintHandler(int signum)
     halt = true;
 }
 
+const float rotationSpeedRadPerSec = 0.5f;
+
 int main(void) {
     signal(SIGINT, sigintHandler);
 
-    MatrixScreen screen(2,1);
-    MatrixDriver matrixDriver(spidev, screen, 1);
-    screen.resetScreen(MatrixScreen::PixelColor::off);
-    screen(1,1) = MatrixScreen::PixelColor::on;
-    screen(2,1) = MatrixScreen::PixelColor::on;
-    screen(3,1) = MatrixScreen::PixelColor::on;
-    screen(4,1) = MatrixScreen::PixelColor::on;
-    screen(5,1) = MatrixScreen::PixelColor::on;
+    MatrixScreen screen(1,1);
+    MatrixDriver matrixDriver(spidev, screen, 24);
+    Gfx2D::Canvas canvas(screen, Gfx2D::CanvasDrawable::Color::off);
 
-    GameOfLife life(screen);
-    matrixDriver.setScreen(screen);
+    std::chrono::time_point<std::chrono::steady_clock> lastWakeTime = std::chrono::steady_clock::now();
+
+    std::vector<Gfx2D::SolidColorTriangle> vec {
+        Gfx2D::SolidColorTriangle({-0.75, -0.75}, {-0.75, 0.75}, {0.75, -0.75}, Gfx2D::CanvasDrawable::Color::on),
+        Gfx2D::SolidColorTriangle({0.75, 0.75}, {-0.75, 0.75}, {0.75, -0.75}, Gfx2D::CanvasDrawable::Color::on)
+    };
+
+    float totalRotationRad = 0.0f;
 
     while(!halt) {
-        matrixDriver.setScreen(life.nextframe());
+        std::chrono::time_point<std::chrono::steady_clock> curTime = std::chrono::steady_clock::now();
+        std::chrono::duration<float> diffInSec = curTime - lastWakeTime;
+        lastWakeTime = curTime;
+        float rotationRad = rotationSpeedRadPerSec * diffInSec.count();
+        totalRotationRad += rotationRad;
+        if (totalRotationRad >= 2*M_PI) {
+            totalRotationRad -= 2*M_PI;
+        }
+        Gfx2D::TransformationMatrix rotation = Gfx2D::createRotationMatrix(totalRotationRad);
+        std::vector<Gfx2D::SolidColorTriangle> transformedVec;
+        // Weird two step thing here, but we are kindof stuck.
+        // We transform the original objects into rotated ones. Someone has to hold on to those
+        // Canvas cannot do it, since canvas only holds references to some interface and knows
+        // nothing about the actual object. Therefore, store it in some new vector, transformedVec.
+        // Butttt, push_back could invalidate old references if a resize triggers.
+        // Therefore, two step: first push them all to some vector, then store references to the contents
+        // of that vector in canvas. No memory issues, but very ugly code.
+        for (auto& v : vec) {
+            transformedVec.push_back(v.createTransformedTriangle(rotation));
+        }
+        for (auto& v : transformedVec) {
+            canvas.addToFrame(v);
+        }
+        matrixDriver.setScreen(canvas.generateFrame());
     }
     return EXIT_SUCCESS;
 }
