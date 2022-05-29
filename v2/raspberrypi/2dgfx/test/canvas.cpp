5dbd75f257bfca997724f723db463d82a5b3c355
Jacko Dirks <jacko.dirks@gmail.com>
2022-05-29 13:34:35 +0200

    v2:rpi:2dgfx: canvas test: Add empty frame, multiple layouts

--- v2/raspberrypi/2dgfx/test/canvas.cpp
+++ v2/raspberrypi/2dgfx/test/canvas.cpp
@@ -149,3 +149,149 @@ TEST(canvas, visibleOverlappingSquares)
 
     EXPECT_EQ(screen, output);
 }
+
+TEST(canvas, canvasShouldEmptyFrame)
+{
+    float pixelDelta = 1.0f/8.0f;
+    std::vector<SolidColorSquareDrawable> vec = {
+        SolidColorSquareDrawable(-1.0, -1.0 + pixelDelta, -1.0, -1.0 + pixelDelta, Gfx2D::CanvasDrawable::Color::on)
+    };
+    MatrixScreen screen(1,1);
+    Gfx2D::Canvas canvas = Gfx2D::Canvas(screen, Gfx2D::CanvasDrawable::Color::off);
+
+    for (auto& drawable : vec) {
+        canvas.addToFrame(drawable);
+    }
+    MatrixScreen output = canvas.generateFrame();
+    screen.resetScreen(MatrixScreen::PixelColor::off);
+    screen(0,0) = MatrixScreen::PixelColor::on;
+
+    EXPECT_EQ(screen, output);
+    output = canvas.generateFrame();
+    screen.resetScreen(MatrixScreen::PixelColor::off);
+    EXPECT_EQ(screen, output);
+}
+
+TEST(canvas, differentMatrixLayouts)
+{
+    // This transformation matrix is good enough for our purposes, but we
+    // do need to keep in mind that it is mostly incorrect.
+    // Since we want the canvas to preserve aspect ratio, we can do this as long as
+    // either width or height is exactly 1 matrix (8 pixels).
+    // In practice, we define a mapping function where (4,4) maps to (0,0) and (1,1) maps to (0.25, 0.25).
+    // The clipping space is then a subsection of this mapping, as long as either width or height is
+    // 8 pixels.
+    Gfx2D::TransformationMatrix translationMat = Gfx2D::createTranslationMatrix(-4, -4);
+    Gfx2D::TransformationMatrix scaleMat = Gfx2D::createScaleMatrix(0.25f, 0.25f);
+    Gfx2D::TransformationMatrix pixelTransformation = scaleMat*translationMat;
+    std::vector<SolidColorSquareDrawable> vec;
+    Gfx2D::Point bottomLeft, topRight;
+
+    // 5 cubes, one in the center and half a cube for every ledmatrix away from the center.
+    // First, the center cube
+    bottomLeft = pixelTransformation * Gfx2D::Point(2, 2);
+    topRight = pixelTransformation * Gfx2D::Point(6,6);
+    vec.emplace_back(bottomLeft, topRight, Gfx2D::CanvasDrawable::Color::on);
+    // Top cube
+    bottomLeft = pixelTransformation * Gfx2D::Point(2, 14);
+    topRight = pixelTransformation * Gfx2D::Point(6, 18);
+    vec.emplace_back(bottomLeft, topRight, Gfx2D::CanvasDrawable::Color::on);
+    // Bottom cube
+    bottomLeft = pixelTransformation * Gfx2D::Point(2, -10);
+    topRight = pixelTransformation * Gfx2D::Point(6, -6);
+    vec.emplace_back(bottomLeft, topRight, Gfx2D::CanvasDrawable::Color::on);
+    // Left cube
+    bottomLeft = pixelTransformation * Gfx2D::Point(-10, 2);
+    topRight = pixelTransformation * Gfx2D::Point(-6, 6);
+    vec.emplace_back(bottomLeft, topRight, Gfx2D::CanvasDrawable::Color::on);
+    // Right cube
+    bottomLeft = pixelTransformation * Gfx2D::Point(14, 2);
+    topRight = pixelTransformation * Gfx2D::Point(18, 6);
+    vec.emplace_back(bottomLeft, topRight, Gfx2D::CanvasDrawable::Color::on);
+
+    // First of all, a 1x1 ledscreen
+    MatrixScreen screen(1,1);
+    Gfx2D::Canvas canvas(screen, Gfx2D::CanvasDrawable::Color::off);
+    for (auto& drawable : vec) {
+        canvas.addToFrame(drawable);
+    }
+    MatrixScreen output = canvas.generateFrame();
+    screen.resetScreen(MatrixScreen::PixelColor::off);
+    for (size_t x = 2; x < 6; ++x) {
+        for (size_t y = 2; y < 6; ++y) {
+            screen(x, y) = MatrixScreen::PixelColor::on;
+        }
+    }
+    EXPECT_EQ(screen, output);
+
+    // Next, 3 wide, 1 high.
+    screen = MatrixScreen(3, 1);
+    canvas = Gfx2D::Canvas(screen, Gfx2D::CanvasDrawable::Color::off);
+    for (auto& drawable : vec) {
+        canvas.addToFrame(drawable);
+    }
+    output = canvas.generateFrame();
+    screen.resetScreen(MatrixScreen::PixelColor::off);
+    // Draw the cube on the left
+    for (size_t x = 0; x < 2; ++x) {
+        for (size_t y = 2; y < 6; ++y) {
+            screen(x, y) = MatrixScreen::PixelColor::on;
+        }
+    }
+    // Draw the center cube
+    for (size_t x = 10; x < 14; ++x) {
+        for (size_t y = 2; y < 6; ++y) {
+            screen(x, y) = MatrixScreen::PixelColor::on;
+        }
+    }
+    // Draw the right cube
+    for (size_t x = 22; x < 24; ++x) {
+        for (size_t y = 2; y < 6; ++y) {
+            screen(x, y) = MatrixScreen::PixelColor::on;
+        }
+    }
+    EXPECT_EQ(screen, output);
+
+    // Then, 1 wide, 3 high
+    screen = MatrixScreen(1, 3);
+    canvas = Gfx2D::Canvas(screen, Gfx2D::CanvasDrawable::Color::off);
+    for (auto& drawable : vec) {
+        canvas.addToFrame(drawable);
+    }
+    output = canvas.generateFrame();
+    screen.resetScreen(MatrixScreen::PixelColor::off);
+    // Draw the cube on the bottom
+    for (size_t x = 2; x < 6; ++x) {
+        for (size_t y = 0; y < 2; ++y) {
+            screen(x, y) = MatrixScreen::PixelColor::on;
+        }
+    }
+    // Draw the center cube
+    for (size_t x = 2; x < 6; ++x) {
+        for (size_t y = 10; y < 14; ++y) {
+            screen(x, y) = MatrixScreen::PixelColor::on;
+        }
+    }
+    // Draw the top cube
+    for (size_t x = 2; x < 6; ++x) {
+        for (size_t y = 22; y < 24; ++y) {
+            screen(x, y) = MatrixScreen::PixelColor::on;
+        }
+    }
+    EXPECT_EQ(screen, output);
+
+    // Finally, a 3x3 screen. Since aspect ratio is preserved, we expect a bigger 1x1,
+    // so only the center cube visible.
+    screen = MatrixScreen(3, 3);
+    canvas = Gfx2D::Canvas(screen, Gfx2D::CanvasDrawable::Color::off);
+    for (auto& drawable : vec) {
+        canvas.addToFrame(drawable);
+    }
+    output = canvas.generateFrame();
+    screen.resetScreen(MatrixScreen::PixelColor::off);
+    for (size_t x = 6; x < 18; ++x) {
+        for (size_t y = 6; y < 18; ++y) {
+            screen(x, y) = MatrixScreen::PixelColor::on;
+        }
+    }
+}
