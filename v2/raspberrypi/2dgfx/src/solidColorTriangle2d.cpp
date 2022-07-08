6db1af05ac9913e3e0b25a03d61d24265b2b7703
Jacko Dirks <jacko.dirks@gmail.com>
2022-07-08 10:23:36 +0200

    v2:rpi:SolidColorTriangle: Remove whitespace

--- v2/raspberrypi/2dgfx/src/solidColorTriangle2d.cpp
+++ v2/raspberrypi/2dgfx/src/solidColorTriangle2d.cpp
@@ -1,15 +1,15 @@
 #include "solidColorTriangle2d.hpp"
 
 Gfx2D::SolidColorTriangle::SolidColorTriangle(const Gfx2D::Point& A, const Gfx2D::Point& B, const Gfx2D::Point& C,
-        const PixelColor& color) : 
+        const PixelColor& color) :
     Gfx2D::Triangle(A, B, C), color(color) {}
 
-Gfx2D::SolidColorTriangle::SolidColorTriangle(const Gfx2D::Triangle& triangle, const PixelColor& color) : 
+Gfx2D::SolidColorTriangle::SolidColorTriangle(const Gfx2D::Triangle& triangle, const PixelColor& color) :
     Gfx2D::Triangle(triangle), color(color) {}
 
 bool Gfx2D::SolidColorTriangle::pointIsInDrawable(const Gfx2D::Point& point) const
 {
-   return this->pointIsInTriangle(point); 
+   return this->pointIsInTriangle(point);
 }
 
 PixelColor Gfx2D::SolidColorTriangle::colorAtPoint(const Gfx2D::Point& /*unused*/) const
