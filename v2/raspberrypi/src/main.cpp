2239a00bbfcc6dedf6c51a5fdb7416749704cc7a
Jacko Dirks <jacko.dirks@gmail.com>
2022-07-11 18:01:10 +0200

    v2:rpi:Update main to cycle through font

--- v2/raspberrypi/src/main.cpp
+++ v2/raspberrypi/src/main.cpp
@@ -5,6 +5,8 @@
 
 #include "matrixScreen.hpp"
 #include "matrixDriver.hpp"
+#include "fontToTexture2d.hpp"
+#include "texturedTriangle2d.hpp"
 
 #include "canvas2d.hpp"
 #include "solidColorTriangle2d.hpp"
@@ -17,46 +19,44 @@ static void sigintHandler(int signum)
     halt = true;
 }
 
-const float rotationSpeedRadPerSec = 0.5f;
-
 int main(void) {
     signal(SIGINT, sigintHandler);
 
-    MatrixScreen screen(1,1);
-    MatrixDriver matrixDriver(spidev, screen, 24);
+    MatrixScreen screen(4,4);
+    MatrixDriver matrixDriver(spidev, screen, 60);
     Gfx2D::Canvas canvas(screen, PixelColor::off);
 
     std::chrono::time_point<std::chrono::steady_clock> lastWakeTime = std::chrono::steady_clock::now();
 
-    std::vector<Gfx2D::SolidColorTriangle> vec {
-        Gfx2D::SolidColorTriangle({-0.75, -0.75}, {-0.75, 0.75}, {0.75, -0.75}, PixelColor::on),
-        Gfx2D::SolidColorTriangle({0.75, 0.75}, {-0.75, 0.75}, {0.75, -0.75}, PixelColor::on)
+    std::size_t fontIndex = 0;
+    Gfx2D::Texture texture = Gfx2D::fontToTexture(fontIndex);
+    std::vector<Gfx2D::TexturedTriangle> vec4 {
+        Gfx2D::TexturedTriangle({-0.75, -0.75}, {-0.75, 0.75}, {0.75, -0.75}, texture, {{0,0}, {1,0}, {0,1}}),
+        Gfx2D::TexturedTriangle({0.75, 0.75}, {-0.75, 0.75}, {0.75, -0.75}, texture, {{1,1}, {1,0}, {0,1}})
     };
 
-    float totalRotationRad = 0.0f;
+    float totalTimePassed = 0.0f;
 
     while(!halt) {
         std::chrono::time_point<std::chrono::steady_clock> curTime = std::chrono::steady_clock::now();
         std::chrono::duration<float> diffInSec = curTime - lastWakeTime;
         lastWakeTime = curTime;
-        float rotationRad = rotationSpeedRadPerSec * diffInSec.count();
-        totalRotationRad += rotationRad;
-        if (totalRotationRad >= 2*M_PI) {
-            totalRotationRad -= 2*M_PI;
+        totalTimePassed += diffInSec.count();
+
+        if (totalTimePassed > 1.0f) {
+            totalTimePassed -= 1.0f;
+            fontIndex++;
+            if (fontIndex > Gfx2D::getMaxFontIndex()) {
+                fontIndex = 0;
+            }
+            texture = Gfx2D::fontToTexture(fontIndex);
+            vec4 = {
+                Gfx2D::TexturedTriangle({-0.75, -0.75}, {-0.75, 0.75}, {0.75, -0.75}, texture, {{0,0}, {1,0}, {0,1}}),
+                Gfx2D::TexturedTriangle({0.75, 0.75}, {-0.75, 0.75}, {0.75, -0.75}, texture, {{1,1}, {1,0}, {0,1}})
+            };
+
         }
-        Gfx2D::TransformationMatrix rotation = Gfx2D::createRotationMatrix(totalRotationRad);
-        std::vector<Gfx2D::SolidColorTriangle> transformedVec;
-        // Weird two step thing here, but we are kindof stuck.
-        // We transform the original objects into rotated ones. Someone has to hold on to those
-        // Canvas cannot do it, since canvas only holds references to some interface and knows
-        // nothing about the actual object. Therefore, store it in some new vector, transformedVec.
-        // Butttt, push_back could invalidate old references if a resize triggers.
-        // Therefore, two step: first push them all to some vector, then store references to the contents
-        // of that vector in canvas. No memory issues, but very ugly code.
-        for (auto& v : vec) {
-            transformedVec.push_back(v.createTransformedTriangle(rotation));
-        }
-        for (auto& v : transformedVec) {
+        for (auto& v : vec4) {
             canvas.addToFrame(v);
         }
         matrixDriver.setScreen(canvas.generateFrame());
