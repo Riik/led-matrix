331b07f6f4b4c46a9f1fd22b6af32a2fb8baa519
Jacko Dirks <jacko.dirks@gmail.com>
2022-07-11 20:42:16 +0200

    v2:rpi:main.cpp: Rolling text

--- v2/raspberrypi/src/main.cpp
+++ v2/raspberrypi/src/main.cpp
@@ -26,38 +26,47 @@ int main(void) {
     MatrixDriver matrixDriver(spidev, screen, 60);
     Gfx2D::Canvas canvas(screen, PixelColor::off);
 
+    const std::string text = "Hoi, Rik!";
+    // Translate the text into a bunch of textures
+    std::vector<Gfx2D::Texture> textTextures;
+    for (const char &ch : text) {
+        textTextures.emplace_back(Gfx2D::fontToTexture(ch));
+    }
+    // Now we need a bunch of texturedTriangles to draw the text on
+    float currentXCoordinate = -1.0f;
+    std::vector<Gfx2D::TexturedTriangle> textTriangles;
+    for (const Gfx2D::Texture& texture : textTextures) {
+        textTriangles.push_back(Gfx2D::TexturedTriangle({currentXCoordinate, -1}, {currentXCoordinate, 1}, {currentXCoordinate + 2.0f, -1},
+                texture, {{0,0}, {0,1}, {1,0}}));
+        textTriangles.push_back(Gfx2D::TexturedTriangle({currentXCoordinate + 2.0f, 1}, {currentXCoordinate, 1}, {currentXCoordinate + 2.0f, -1},
+                texture, {{1,1}, {0,1}, {1,0}}));
+        currentXCoordinate += 2.0f;
+    }
+    currentXCoordinate += 1.0f;
+
     std::chrono::time_point<std::chrono::steady_clock> lastWakeTime = std::chrono::steady_clock::now();
-
-    std::size_t fontIndex = 0;
-    Gfx2D::Texture texture = Gfx2D::fontToTexture(fontIndex);
-    std::vector<Gfx2D::TexturedTriangle> vec4 {
-        Gfx2D::TexturedTriangle({-0.75, -0.75}, {-0.75, 0.75}, {0.75, -0.75}, texture, {{0,0}, {1,0}, {0,1}}),
-        Gfx2D::TexturedTriangle({0.75, 0.75}, {-0.75, 0.75}, {0.75, -0.75}, texture, {{1,1}, {1,0}, {0,1}})
-    };
-
-    float totalTimePassed = 0.0f;
+    constexpr float textSpeedPerSec = 2.0f;
+    constexpr float startPosition = -2.0f;
+    float totalMovement = startPosition;
 
     while(!halt) {
         std::chrono::time_point<std::chrono::steady_clock> curTime = std::chrono::steady_clock::now();
         std::chrono::duration<float> diffInSec = curTime - lastWakeTime;
         lastWakeTime = curTime;
-        totalTimePassed += diffInSec.count();
-
-        if (totalTimePassed > 1.0f) {
-            totalTimePassed -= 1.0f;
-            fontIndex++;
-            if (fontIndex > Gfx2D::getMaxFontIndex()) {
-                fontIndex = 0;
-            }
-            texture = Gfx2D::fontToTexture(fontIndex);
-            vec4 = {
-                Gfx2D::TexturedTriangle({-0.75, -0.75}, {-0.75, 0.75}, {0.75, -0.75}, texture, {{0,0}, {1,0}, {0,1}}),
-                Gfx2D::TexturedTriangle({0.75, 0.75}, {-0.75, 0.75}, {0.75, -0.75}, texture, {{1,1}, {1,0}, {0,1}})
-            };
-
+        totalMovement += diffInSec.count() * textSpeedPerSec;
+        if (totalMovement > currentXCoordinate) {
+            totalMovement = startPosition;
         }
-        for (auto& v : vec4) {
-            canvas.addToFrame(v);
+        Gfx2D::TransformationMatrix translation = Gfx2D::createTranslationMatrix(-totalMovement, 0.0f);
+
+        std::vector<Gfx2D::TexturedTriangle> transformedTriangles;
+
+        for (const Gfx2D::TexturedTriangle& triangle : textTriangles) {
+            transformedTriangles.emplace_back(triangle.createTransformedTriangle(translation));
+        }
+
+        for (const Gfx2D::TexturedTriangle& triangle : transformedTriangles) {
+            canvas.addToFrame(triangle);
         }
         matrixDriver.setScreen(canvas.generateFrame());
     }
