b9e500abcca4a0cd464b716d91249eaa9e10ce8b
Rik van der Vlist <rikvandervlist@eneco.com>
2022-07-17 23:20:20 +0200

    first buggy version

--- v2/raspberrypi/src/matrixDriver.cpp
+++ v2/raspberrypi/src/matrixDriver.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 
 #include "matrixDriver.hpp"
+#include <ncurses.h>
 
 
 MatrixDriver::MatrixDriver(const std::string &spiDevName, const MatrixScreen &exampleScreen,
@@ -16,19 +17,35 @@ MatrixDriver::MatrixDriver(const std::string &spiDevName, const MatrixScreen &ex
     screen{exampleScreen}, matrixCountWidth{exampleScreen.getMatrixCountWidth()}, matrixCountHeight{exampleScreen.getMatrixCountHeight()},
     physicalConnectionLocation{physicalConnectionLocation}, newDataAvailable{0}
 {
-
-    std::cout << "Matridriver started" << std::endl;
-
+    initscr();             // initialize curses
+    cbreak();              // pass key presses to program, but not signals
+    noecho();              // don't echo key presses to screen
+    keypad(stdscr, TRUE);  // allow arrow keys
+    // timeout(0);            // no blocking on getch()
+    curs_set(0);           // set the cursor to invisible
+    // std::cout << "Matridriver started" << std::endl;
     this->outputWorker = std::jthread(std::bind_front(&MatrixDriver::screenToSpi, this));
 }
 
 MatrixDriver::~MatrixDriver()
 {
     // close(this->spifd);
+    endwin();			/* End curses mode		  */
 }
 
 void MatrixDriver::setScreen(MatrixScreen screen)
 {
+  const std::size_t matrixCount = this->matrixCountWidth * this->matrixCountHeight;
+
+  std::array<std::vector<uint8_t>, 8> matrixContentBuf;
+  for (std::size_t i = 0; i < 8; ++i) {
+      matrixContentBuf[i] = std::vector<uint8_t>(matrixCount*2, 0);
+      for (std::size_t j = 0; j < matrixContentBuf[i].size(); j += 2) {
+          matrixContentBuf[i][j] = i+1;
+      }
+  }
+
+  // std::cout << "setting screen" << std::endl;
 #if defined(DEBUG)
     if (screen.getMatrixCountWidth() != this->screen.getMatrixCountWidth()) {
         std::stringstream ss;
@@ -42,6 +59,39 @@ void MatrixDriver::setScreen(MatrixScreen screen)
         throw std::out_of_range(ss.str());
     }
 #endif //defined(DEBUG)
+
+    for (std::size_t matCol = 0; matCol < this->screen.getMatrixCountWidth(); ++matCol) {
+        for (std::size_t matRow = 0; matRow < this->screen.getMatrixCountHeight(); ++matRow) {
+            // Isolate the pixelcoordinates that belong to this matrix
+            std::size_t pixelRowMin;
+            if (this->physicalConnectionLocation == MatrixDriver::PhysicalConnectionLocation::topLeft) {
+                pixelRowMin = this->screen.getPixelCountHeight() - (matRow + 1)*MatrixScreen::matrixPixelCountHeight;
+            } else {
+                pixelRowMin = matRow*MatrixScreen::matrixPixelCountHeight;
+            }
+            std::size_t pixelColMin = matCol*MatrixScreen::matrixPixelCountWidth;
+
+            std::size_t curIndex = matrixCount - 1 - (matRow * this->matrixCountWidth + matCol);
+            // Finally, we set the relevant bytes.
+            // A buffer is about a column.
+            for (std::size_t i = 0; i < MatrixScreen::matrixPixelCountWidth; ++i) {
+                uint8_t &ref = matrixContentBuf[i][2*curIndex + 1];
+                ref = 0;
+                move(i,0);
+                for (std::size_t j = 0; j < MatrixScreen::matrixPixelCountHeight; ++j) {
+                    // std::cout << "..";
+                    if (this->screen(pixelColMin + i, pixelRowMin + j) == PixelColor::on) {
+                        addch('.' | A_REVERSE);
+                        addch('.' | A_REVERSE);
+                    } else {
+                        addch('.');
+                  			addch('.');
+                    }
+                }
+            }
+        }
+    }
+    refresh();
 
     this->screenMutex.lock();
     this->screen = std::move(screen);
