9584db385f6d4299e765796dbe3faf86b8b75fe2
Jacko Dirks <jacko.dirks@gmail.com>
2022-08-14 16:28:06 +0200

    v2:rpi:boundaryBox2d: Assert box layout

--- v2/raspberrypi/2dgfx/src/boundaryBox2d.cpp
+++ v2/raspberrypi/2dgfx/src/boundaryBox2d.cpp
@@ -1,8 +1,13 @@
+#include <cassert>
+
 #include "boundaryBox2d.hpp"
 
 Gfx2D::BoundaryBox::BoundaryBox() {}
 
-Gfx2D::BoundaryBox::BoundaryBox(Gfx2D::Point high, Gfx2D::Point low) : high(std::move(high)), low(std::move(low)) {}
+Gfx2D::BoundaryBox::BoundaryBox(Gfx2D::Point high, Gfx2D::Point low) : high(std::move(high)), low(std::move(low)) {
+    assert(this->high.x() >= this->low.x());
+    assert(this->high.y() >= this->low.y());
+}
 
 bool Gfx2D::BoundaryBox::overlapsWith(const Gfx2D::BoundaryBox& other) const
 {
