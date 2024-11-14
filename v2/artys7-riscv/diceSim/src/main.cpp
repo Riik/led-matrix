5aaedc6cd86804da0975abdf4e4b4b6854771952
Jacko Dirks <jacko.dirks@gmail.com>
2024-11-14 22:45:13 +0100

    v2 riscv: Rotate output

--- v2/artys7-riscv/diceSim/src/main.cpp
+++ v2/artys7-riscv/diceSim/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 #include "hal/inc/uart.h"
 #include "common/inc/matrixScreen.hpp"
@@ -38,8 +39,9 @@ static MatrixScreen screenFromNumber(Gfx2D::Canvas& canvas, uint_fast8_t num) {
     }
     float translationX = num < 10 ? 0.2f : -0.75f;
     Gfx2D::TransformationMatrix translationMatrix = Gfx2D::createTranslationMatrix(translationX, 0.0f);
+    Gfx2D::TransformationMatrix rotateMatrix = Gfx2D::createRotationMatrix(M_PI);
     Gfx2D::TransformationMatrix scaleMatrix = Gfx2D::createScaleMatrix(0.5f, 0.5f);
-    Gfx2D::TransformationMatrix transformation = scaleMatrix * translationMatrix;
+    Gfx2D::TransformationMatrix transformation = scaleMatrix * rotateMatrix * translationMatrix;
     std::vector<Gfx2D::TexturedTriangle> transformedTriangles;
 
     for (const Gfx2D::TexturedTriangle& triangle : textTriangles) {
